894854e4bc49ae2e09bfe8bcd72244be94f43951
snnbotchway <solomonbotchway7@gmail.com>
2023-04-09 13:23:31 +0000

    Write compile script

---
+++ ethereum/compile.ts
@@ -0,0 +1,35 @@
+import path from "path";
+import fs from "fs-extra";
+import solc from "solc";
+
+const contractFile = "Campaign.sol";
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath);
+
+const campaignPath = path.resolve(__dirname, "contracts", contractFile);
+const source = fs.readFileSync(campaignPath, "utf8");
+const input = {
+	language: "Solidity",
+	sources: {
+		[contractFile]: {
+			content: source,
+		},
+	},
+	settings: {
+		outputSelection: {
+			"*": {
+				"*": ["abi", "evm.bytecode"],
+			},
+		},
+	},
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+const contracts = output.contracts[contractFile];
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in contracts) {
+	fs.outputJSONSync(path.resolve(buildPath, `${contract}.json`), contracts[contract]);
+}
