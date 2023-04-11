f4b468313edf990c7ab1caaae516bd03604a73f6
snnbotchway <solomonbotchway7@gmail.com>
2023-04-11 22:52:59 +0000

    Create initial layout

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,3 +1,13 @@
+import React from "react";
+import "./globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Header from "../components/Header";
+import Container from "../components/Container";
+import ThemeProvider from "../theme";
+
 export const metadata = {
 	title: "Incite",
 };
@@ -5,7 +15,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<ThemeProvider>
+					<Header />
+					<Container maxWidth="md">{children}</Container>
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
